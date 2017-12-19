292ceaf8683c42459ed2c4748d03bf3f8914dcda
Martijn van Maurik <git@vmaurik.nl>
2017-12-19 14:30:38 +0100

    Add tmux on SSH connections

--- fish/.config/fish/conf.d/00-ssh-init.fish
+++ fish/.config/fish/conf.d/00-ssh-init.fish
@@ -1,6 +1,8 @@
-if type -qf tmux
-  if test -z "$TMUX"; and test "$SSH_CONNECTION" != ""
-    tmux attach-session -t "$USER-tmux"; or tmux new-session -s "$USER-tmux"
-    exit
-  end
+if type -qf tmux                         
+  if status --is-login                   
+    if test -z "$TMUX"; and test "$SSH_CONNECTION" != ""                           
+      tmux attach-session -t "$USER-tmux"; or tmux new-session -s "$USER-tmux"     
+      exec true                          
+    end                                  
+  end                                    
 end
