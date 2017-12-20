37c9a0502cfebad4ba3ef86d0f1ec7dc06aff8d2
Martijn van Maurik <git@vmaurik.nl>
2017-12-20 09:15:20 +0100

    Use a bit of logic

--- fish/.config/fish/conf.d/00-ssh-init.fish
+++
@@ -1,8 +0,0 @@
-if type -qf tmux                         
-  if status --is-login                   
-    if test -z "$TMUX"; and test "$SSH_CONNECTION" != ""                           
-      tmux attach-session -t "$USER-tmux"; or tmux new-session -s "$USER-tmux"     
-      exec true                          
-    end                                  
-  end                                    
-end
