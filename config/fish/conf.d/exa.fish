4eb8baaba8131919b9d1b83e0f4a0a0e81e46c5c
Martijn van Maurik <git@vmaurik.nl>
2017-12-28 15:45:11 +0100

    Refactor a bit again and again

--- config/fish/conf.d/exa.fish
+++ config/fish/conf.d/exa.fish
@@ -2,6 +2,6 @@ function ls --description 'List contents of directory'
   if type -qf exa
     eval exa $argv
   else
-    /usr/bin/ls --color=auto $argv
+    eval /usr/bin/ls --color=auto $argv
   end
 end
--- config/fish/conf.d/paths.fish
+++ config/fish/conf.d/paths.fish
@@ -1,4 +1,4 @@
 # set paths
 test -d ~/bin/; and set -U fish_user_paths ~/bin $fish_user_paths
 test -d ~/go/bin/; and set -U fish_user_paths ~/go/bin $fish_user_paths
-test -d ~/.cargo/bin/; and set -U fish_user_paths ~/.cargo/bin/ $fish_user_paths
+#test -d ~/.cargo/bin/; and set -U fish_user_paths ~/.cargo/bin/ $fish_user_paths
