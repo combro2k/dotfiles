7e12add2f5d02e1e0dd2f0999ea8526bba761b1f
Martijn van Maurik <git@vmaurik.nl>
2017-12-28 16:22:24 +0100

    Fix ls placement of debian and opensuse

--- config/fish/conf.d/exa.fish
+++ config/fish/conf.d/exa.fish
@@ -2,6 +2,7 @@ function ls --description 'List contents of directory'
   if type -qf exa
     eval exa $argv
   else
-    eval /usr/bin/ls --color=auto $argv
+    set -l ls (which ls)
+    eval $ls --color=auto $argv
   end
 end
