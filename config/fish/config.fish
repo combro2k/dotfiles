2b246fe8405f1012573b3be29a398ff10852d6f5
Martijn van Maurik <git@vmaurik.nl>
2018-01-10 17:19:07 +0100

    Remove anoying message about dbus in GTK apps

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,7 @@
 # Global settings
 set -g -x SXHKD_SHELL '/usr/bin/sh'
 set -g -x TERMINAL '/usr/bin/urxvt'
+set -g -x NO_AT_BRIDGE '1'
 
 # Use tmux when it's installed and is an SSH connection
 if type -qf tmux
