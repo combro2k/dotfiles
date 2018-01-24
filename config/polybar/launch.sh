5e5e5275546c40d6d461e676f713bf1e7abb788c
Martijn van Maurik <git@vmaurik.nl>
2018-01-24 11:43:18 +0100

    Launch bar at each display ..

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,7 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar default > /dev/null 2> /dev/null &
+# Launch an bar at each display
+export -a MONITOR
+for MONITOR in $(bspc query -M --names); do
+  polybar default > /dev/null 2> /dev/null &
+done
 
 echo "Bars launched..."
