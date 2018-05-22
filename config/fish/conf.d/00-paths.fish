3c15bdd3b9c07344b9050eaa9d5f25c50b584c47
Martijn van Maurik <git@vmaurik.nl>
2018-05-22 13:28:24 +0200

    Fix loading issues

--- config/fish/conf.d/00-paths.fish
+++ config/fish/conf.d/00-paths.fish
@@ -1,5 +1,8 @@
 # set paths
-test -d ~/bin/; and set -x fish_user_paths ~/bin $fish_user_paths
-test -d ~/.local/bin/; and set -x fish_user_paths ~/.local/bin $fish_user_paths
-test -d ~/go/bin/; and set -x fish_user_paths ~/go/bin $fish_user_paths
-test -d ~/.cargo/bin/; and set -x fish_user_paths ~/.cargo/bin/ $fish_user_paths
+
+if status --is-interactive
+  test -d ~/bin; and if not contains ~/bin $PATH; echo "Append ~/bin to PATH"; set -U fish_user_paths ~/bin $fish_user_paths; end
+  test -d ~/.local/bin/; and if not contains ~/.local/bin $PATH; echo "Append ~/.local/bin to PATH"; set -U fish_user_paths ~/.local/bin $fish_user_paths; end
+  test -d ~/go/bin; and if not contains ~/go/bin $PATH; echo "Append ~/go/bin to PATH"; set -U fish_user_paths ~/go/bin $fish_user_paths; end
+  test -d ~/.cargo/bin/; and if not contains ~/.cargo/bin/ $PATH; echo "Append ~/.cargo/bin to PATH"; set -U fish_user_paths ~/.cargo/bin/ $fish_user_paths; end
+end
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,5 @@
 # Global settings
+
 set -g -x SXHKD_SHELL '/usr/bin/sh'
 set -g -x TERMINAL '/usr/bin/urxvt'
 set -g -x NO_AT_BRIDGE '1'
