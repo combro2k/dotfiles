416639ae786188ad92139a934c85d9cdbff5a023
Martijn van Maurik <git@vmaurik.nl>
2018-05-24 17:02:47 +0200

    WIP

--- config/fish/functions/emacs.fish
+++ config/fish/functions/emacs.fish
@@ -1,7 +1,15 @@
 function emacs --description 'Star emacs'
-  if type -qf emacs-nox
+  if not type -qf emacs; and not type -qf emacs-nox
+    echo "Emacs is not installed, install it first!"
+
+    return 1
+  end
+
+  if set -q DISPLAY
+    eval /usr/bin/emacs $argv
+  else if type -qf emacs-nox
     eval /usr/bin/emacs-nox $argv
   else
-    /usr/bin/emacs $argv
-  end
+    eval /usr/bin/emacs $argv
+ end
 end
