a845f7665ab3efdd8e4d419d6f971b302fb3dcb6
Martijn van Maurik <git@vmaurik.nl>
2018-07-06 17:17:00 +0200

    cleanup

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,15 +1,16 @@
 function fish_greeting
-  if test -z $SUDO_USER
-    if not set -q TMUX
-      /usr/bin/neofetch
-    else if [ "$TMUX_PANE" = '%1' ]
-      /usr/bin/neofetch --off
-    end
+  set -q SUDO_USER; and return
 
-    test -x /usr/bin/fortune; and /usr/bin/fortune -s
-    test -x /usr/games/fortune; and /usr/games/fortune -s
-    echo ""
-    uname -snrmio
-    uptime
+  if set -q TMUX; and test "$TMUX_PANE" != '%1'
+    return
   end
+
+  set -q TMUX; and /usr/bin/neofetch --off; or /usr/bin/neofetch
+  type -q fortune; and fortune -s
+
+  echo ""
+  uname -snrmio
+  uptime
+
+  return
 end
