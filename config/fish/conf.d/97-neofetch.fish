430caabd8cbbafc59a90562fe847fc4ee81ab90e
Martijn van Maurik <git@vmaurik.nl>
2018-07-06 16:48:38 +0200

    cleanup

--- config/fish/conf.d/97-neofetch.fish
+++
@@ -1,9 +0,0 @@
-if status --is-interactive
-  if test -f /usr/bin/neofetch; and not set -q SUDO_USER
-    if not set -q TMUX
-      /usr/bin/neofetch
-    else if [ "$TMUX_PANE" = '%1' ]
-      /usr/bin/neofetch --off
-    end
-  end
-end
--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,5 +1,11 @@
 function fish_greeting
   if test -z $SUDO_USER
+    if not set -q TMUX
+      /usr/bin/neofetch
+    else if [ "$TMUX_PANE" = '%1' ]
+      /usr/bin/neofetch --off
+    end
+
     test -x /usr/bin/fortune; and /usr/bin/fortune -s
     test -x /usr/games/fortune; and /usr/games/fortune -s
     echo ""
