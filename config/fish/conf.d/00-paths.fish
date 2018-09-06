3f51838cd851052324bd213b09ca6aa06db9e14a
Martijn van Maurik <git@vmaurik.nl>
2018-09-06 09:23:18 +0200

    Add linuxbrew

--- config/fish/conf.d/00-paths.fish
+++ config/fish/conf.d/00-paths.fish
@@ -5,5 +5,6 @@ if status --is-interactive
   test -d ~/.local/bin/; and if not contains ~/.local/bin $PATH; set -x fish_user_paths ~/.local/bin $fish_user_paths; end
   test -d ~/go/bin; and if not contains ~/go/bin $PATH; set -x fish_user_paths ~/go/bin $fish_user_paths; end
   test -d ~/.cargo/bin/; and if not contains ~/.cargo/bin/ $PATH; set -x fish_user_paths ~/.cargo/bin/ $fish_user_paths; end
+  test -d ~/.linuxbrew/bin/; and if not contains ~/.linuxbrew/bin/ $PATH; set -x fish_user_paths ~/.linuxbrew/bin/ $fish_user_paths; end
   #  test -d /snap/bin/; and if not contains /snap/bin $PATH; set -U fish_user_paths /snap/bin/ $fish_user_paths; end
 end
