7882912121839cb89d03363b40006fc5ad3038fd
Martijn van Maurik <git@vmaurik.nl>
2019-02-02 14:48:38 +0100

    Update to vrgb and autodetection screensize

--- config/qtile/screens.py
+++ config/qtile/screens.py
@@ -24,18 +24,18 @@ screens = [
         top=bar.Bar([
                 MenuWidget(
                     background='3badea',
-                    fontsize=36 if screensize['height'] <= 1050 else 48,
+                    fontsize=36 if screensize['height'] <= 1050 else 40,
                     padding=10,
                 ),
                 widget.CurrentLayout(
                     foreground='ffffff',
                     background='585858',
-                    fontsize=13 if screensize['height'] <= 1050 else 15,
+                    fontsize=13 if screensize['height'] <= 1050 else 14,
                     padding=10,
                 ),
                 widget.GroupBox(
-                    padding=0 if screensize['height'] <= 1050 else 7,
-                    fontsize=15 if screensize['height'] <= 1050 else 18,
+                    padding=0 if screensize['height'] <= 1050 else 5,
+                    fontsize=15 if screensize['height'] <= 1050 else 19,
                     highlight_method='block',
                     spacing=0,
                     use_mouse_wheel=False,
@@ -49,7 +49,7 @@ screens = [
                 ),
                 TaskListNew(
                     spacing=0,
-                    padding=0 if screensize['height'] <= 1050 else 7,
+                    padding=0 if screensize['height'] <= 1050 else 5,
                     background='585858',
                     border='3badea',
                     fontsize=16 if screensize['height'] <= 1050 else 19,
@@ -71,17 +71,17 @@ screens = [
                 ),
                 widget.Clock(
                     background='585858',
-                    fontsize=16 if screensize['height'] <= 1050 else 19,
+                    fontsize=16 if screensize['height'] <= 1050 else 20,
                     format='%I:%M %p',
                     padding=10,
                 ),
                 ActionMenuWidget(
                     background='3badea',
-                    fontsize=32 if screensize['height'] <= 1050 else 42,
+                    fontsize=32 if screensize['height'] <= 1050 else 40,
                     padding=10,
                 ),
             ],
-            size=27 if screensize['height'] <= 1050 else 42,
+            size=27 if screensize['height'] <= 1050 else 40,
 #            background='1d1f21',
             background='585858',
             opacity=0.888888880
