7a1fe5e85b7950002f649d8d3864ed3965867cf2
Martijn van Maurik <git@vmaurik.nl>
2019-03-06 12:08:34 +0100

    Fix dotfiles

--- config/fish/conf.d/96-fundle.fish
+++ config/fish/conf.d/96-fundle.fish
@@ -12,16 +12,8 @@ if status --is-interactive
   set -x FZF_LEGACY_KEYBINDINGS 0
 
   # list plugin dependencies
-  fundle plugin 'fisherman/await'
-  fundle plugin 'fisherman/choices'
+  fundle plugin 'fishpkg/fish-await'
   fundle plugin 'tuvistavie/fish-completion-helpers'
-  fundle plugin 'fisherman/get'
-  fundle plugin 'fisherman/getopts'
-  fundle plugin 'fisherman/get_file_age'
-  fundle plugin 'fisherman/last_job_id'
-  fundle plugin 'fisherman/menu'
-
-  # plugins
   fundle plugin 'tuvistavie/oh-my-fish-core'
   fundle plugin 'tuvistavie/fish-completion-helpers'
   fundle plugin 'oh-my-fish/plugin-vcs'
@@ -29,16 +21,8 @@ if status --is-interactive
   fundle plugin 'oh-my-fish/plugin-license'
   fundle plugin 'oh-my-fish/plugin-sudope'
   fundle plugin 'maman/plugin-gvm'
-
-  # fundle plugin 'oh-my-fish/plugin-rustup'
-  fundle plugin 'fisherman/fzf'
-  #fundle plugin 'fisherman/docker-completion'
+  fundle plugin 'jethrokuan/fzf'
   fundle plugin 'edc/bass'
-  #fundle plugin 'oh-my-fish/plugin-autojump'
-
-  # theme
-  # fundle plugin 'oh-my-fish/theme-bira'
-  # fundle plugin 'oh-my-fish/theme-bobthefish'
   fundle plugin 'smh/base16-shell-fish'
   fundle plugin 'combro2k/theme-cmorrell-twilight'
 
