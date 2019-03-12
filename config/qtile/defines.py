59df658abcd5a10c228eb4e89c1854aa915919a7
Martijn van Maurik <git@vmaurik.nl>
2019-03-12 09:15:16 +0100

    Cleanup qtile autostart defines and hook logging

--- config/qtile/defines.py
+++ config/qtile/defines.py
@@ -1,29 +1,15 @@
 def autostart():
     from os.path import expanduser
 
-    #yield '/usr/bin/feh', '--bg-scale', '--randomize', expanduser('~/.config/backgrounds/'), '-Z'
-
     yield '/usr/bin/ibus-daemon', '--xim'
-    
     yield '/usr/bin/conky', '-c', expanduser('~/.config/conky/conky.conf')
-#    yield '/usr/bin/conky', '-c', expanduser('~/.config/conky/conky-shortcuts.conf')
-
     yield '/usr/bin/xautolock', '-time', '10', '-locker', 'xlock -mode blank'
     yield '/usr/bin/urxvtd-256color', '-o', '-q'
     yield '/usr/bin/emacs-nox', '--daemon'
-
-    # broken?
     yield '/usr/bin/nm-applet'
     yield '/usr/bin/package-update-indicator'
-
     yield '/usr/lib/polkit-gnome-authentication-agent-1'
     yield '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1'
-#    yield '/usr/bin/gnome-keyring-daemon', '-f', '--start', '--components=ssh'
-
     yield '/usr/bin/clipit'
-
-#   to resource hungry :-(
-#    yield '/usr/bin/compton', '--dbus'
-
     yield '/usr/bin/vmware-user-suid-wrapper'
-
+    yield '/usr/bin/blueman-applet'
