bcdb7c03e0c07429d9cfe1a78c6211e1a5cf1cf9
Martijn van Maurik <git@vmaurik.nl>
2019-03-19 16:12:08 +0100

    add pinentry-gnome3 support (grc-prompter) and use global pinentry wrapper for gpg-agent

--- config/qtile/hooks.py
+++ config/qtile/hooks.py
@@ -67,3 +67,5 @@ def specific_instance_rules(window): # type: Window
         window.above = True
     if window.match(wmclass='skype'):
         window.cmd_set_size_floating(w=1512,h=800, curx=0, cury=0)
+
+    # logger.error(window.window.get_wm_class())
