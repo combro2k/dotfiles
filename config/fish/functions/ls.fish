6d5faa51f35de00533ee0eca299bee4104528265
Martijn van Maurik <git@vmaurik.nl>
2019-06-05 15:52:50 +0200

    Add default ls -altrn fix, I use that a lot :-P

--- config/fish/functions/ls.fish
+++ config/fish/functions/ls.fish
@@ -1,5 +1,9 @@
 function ls --description 'List contents of directory'
   if type -qf exa
+    if set -q argv; and [ "$argv" = '-altrn' ]
+      set argv "-balgsold"
+     end
+
     eval exa $argv
   else
     set -l ls (which ls)
