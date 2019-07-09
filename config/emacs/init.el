334cf4b02386673c9b8006f61554ee7db4db6e12
Martijn van Maurik <git@vmaurik.nl>
2019-07-09 13:21:58 +0200

    Update dotfiles

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(base16-distinct-fringe-backgeround 0)
  '(coding-system-for-read (quote utf-8) t)
  '(default-fill-column 80)
  '(delete-old-versions -1)
@@ -18,9 +17,9 @@
 
 (require 'package)
 (add-to-list 'package-archives 
-	     '("melpa" . "https://melpa.org/packages/")
-	     '("gnu" . "http://elpa.gnu.org/packages/")
-	     )
+ '("melpa" . "https://melpa.org/packages/")
+ '("gnu" . "http://elpa.gnu.org/packages/")
+)
 
 (package-initialize)
 
@@ -46,13 +45,46 @@
 (use-package base16-theme
   :ensure t
   :init
+  (setq base16-distinct-fringe-backgeround 0)
   (setq base16-theme-256-color-source 'base16-shell)
   (if (file-exists-p "~/.emacs_theme.el") 
-    (load "~/.emacs_theme.el")
+      (load "~/.emacs_theme.el")
     )
   )
 (use-package doom-modeline
   :ensure t
+  :init
+  (setq doom-modeline-icon t)
+  (setq doom-modeline-major-mode-icon t)
+  (setq doom-modeline-buffer-state-icon t)
+  (setq doom-modeline-buffer-modification-icon t)
+  (setq doom-modeline-minor-modes nil)
+  (setq doom-modeline-enable-word-count nil)
+  (setq doom-modeline-buffer-encoding t)
+  (setq doom-modeline-indent-info nil)
+  (setq doom-modeline-checker-simple-format t)
+  (setq doom-modeline-vcs-max-length 12)
+  (setq doom-modeline-persp-name t)
+  (setq doom-modeline-persp-name-icon nil)
+  (setq doom-modeline-lsp t)
+  (setq doom-modeline-github nil)
+  (setq doom-modeline-github-interval (* 30 60))
+  (setq doom-modeline-env-version t)
+  (setq doom-modeline-env-enable-python t)
+  (setq doom-modeline-env-enable-ruby t)
+  (setq doom-modeline-env-enable-perl t)
+  (setq doom-modeline-env-enable-go t)
+  (setq doom-modeline-env-enable-elixir t)
+  (setq doom-modeline-env-enable-rust t)
+  (setq doom-modeline-env-python-executable "python")
+  (setq doom-modeline-env-ruby-executable "ruby")
+  (setq doom-modeline-env-perl-executable "perl")
+  (setq doom-modeline-env-go-executable "go")
+  (setq doom-modeline-env-elixir-executable "iex")
+  (setq doom-modeline-env-rust-executable "rustc")
+  (setq doom-modeline-height 1)
+  (set-face-attribute 'mode-line nil :height 100)
+  (set-face-attribute 'mode-line-inactive nil :height 100)
   :hook (after-init . doom-modeline-mode)
   )
 (use-package helm
@@ -60,5 +92,6 @@
   (helm-mode 1)
   (global-set-key (kbd "M-x") 'helm-M-x)
   )
+(use-package all-the-icons)
 (menu-bar-mode -1)
 (custom-set-faces)
