494357c8392c44844a0462086ac8e21044260884
combro2k <git@vmaurik.nl>
2020-11-09 16:43:30 +0100

    update paths

--- config/fish/conf.d/00-paths.fish
+++ config/fish/conf.d/00-paths.fish
@@ -10,10 +10,12 @@ if status --is-interactive
   # specific paths if exists
   test -d ~/bin/; and set -g -x PATH $PATH ~/bin
   test -d ~/.local/bin/; and set -g -x PATH $PATH ~/.local/bin
-  test -d ~/go/bin/; and set -g -x PATH $PATH ~/go/bin
   test -d ~/.cargo/bin/; and set -g -x PATH $PATH ~/.cargo/bin
   test -d ~/.linuxbrew/bin/; and set -g -x PATH $PATH ~/.linuxbrew/bin
   test -d ~/.fzf/bin/; and set -g -x PATH $PATH ~/.fzf/bin
   test -d ~/.luarocks/bin/; and set -g -x PATH $PATH ~/.luarocks/bin
+  test -d ~/.npm-global/bin/; and set -g -x PATH $PATH ~/.npm-global/bin
+  test -d ~/go/bin/; and set -g -x PATH $PATH ~/go/bin
+
   test -d /snap/bin/; and set -g -x PATH $PATH /snap/bin
 end
