959ddd66d54110af8033461dc2d5b85b02ab88c3
combro2k <git@vmaurik.nl>
2021-03-10 16:35:28 +0100

    update dotfiles

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -1,34 +1,19 @@
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
-
--- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
 require("awful.autofocus")
--- Widget and layout library
 local wibox = require("wibox")
--- Theme handling library
 local beautiful = require("beautiful")
--- Notification library
 local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
-
--- Load Debian menu entries
 local debian = require("debian.menu")
 local has_fdo, freedesktop = pcall(require, "freedesktop")
-
--- dropdown terminal https://gkaczorek.com/2020-09-25-adding-an-alacritty-based-drop-down-terminal-in-awesomewm/
-local quake = require("util/quake")
 local autostart = require("util/autostart")
+local lain = require("lain")
+local dpi = require("beautiful.xresources").apply_dpi
 
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
   naughty.notify(
     {
@@ -39,7 +24,6 @@ if awesome.startup_errors then
   )
 end
 
--- Handle runtime errors after startup
 do
   local in_error = false
   awesome.connect_signal(
@@ -59,35 +43,35 @@ do
     end
   )
 end
--- }}}
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+--    "blackburn",       -- 1
+--    "copland",         -- 2
+--    "dremora",         -- 3
+--    "holo",            -- 4
+--    "multicolor",      -- 5
+--    "powerarrow",      -- 6
+--    "powerarrow-dark", -- 7
+--    "rainbow",         -- 8
+--    "steamburn",       -- 9
+--    "vertex",          -- 10
+local chosen_theme = "powerarrow"
 
--- This is used later as the default terminal and editor to run.
-terminal = "urxvtc"
-editor = os.getenv("EDITOR") or "editor"
-editor_cmd = terminal .. " -e " .. editor
+local terminal = "urxvtc"
+local editor = os.getenv("EDITOR") or "editor"
+local editor_cmd = terminal .. " -e " .. editor
 
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
--- Table of layouts to cover with awful.layout.inc, order matters.
+awful.util.terminal = terminal
+
 awful.layout.layouts = {
   awful.layout.suit.fair,
   awful.layout.suit.fair.horizontal,
   awful.layout.suit.max,
   awful.layout.suit.floating,
+  awful.layout.suit.spiral.dwindle,
 }
--- }}}
 
--- {{{ Menu
--- Create a launcher widget and a main menu
 myawesomemenu = {
   {
     "hotkeys",
@@ -101,6 +85,18 @@ myawesomemenu = {
 
 local menu_awesome = {"awesome", myawesomemenu, beautiful.awesome_icon}
 local menu_terminal = {"open terminal", terminal}
+
+lain.layout.termfair.nmaster           = 3
+lain.layout.termfair.ncol              = 1
+lain.layout.termfair.center.nmaster    = 3
+lain.layout.termfair.center.ncol       = 1
+lain.layout.cascade.tile.offset_x      = dpi(2)
+lain.layout.cascade.tile.offset_y      = dpi(32)
+lain.layout.cascade.tile.extra_padding = dpi(5)
+lain.layout.cascade.tile.nmaster       = 5
+lain.layout.cascade.tile.ncol          = 2
+
+beautiful.init(string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv("HOME"), chosen_theme))
 
 if has_fdo then
   mymainmenu = freedesktop.menu.build(
@@ -122,18 +118,9 @@ mylauncher = awful.widget.launcher(
   {image = beautiful.awesome_icon, menu = mymainmenu}
 )
 
--- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
--- }}}
-
--- Keyboard map indicator and switcher
 mykeyboardlayout = awful.widget.keyboardlayout()
-
--- {{{ Wibar
--- Create a textclock widget
 mytextclock = wibox.widget.textclock()
-
--- Create a wibox for each screen and add it
 local taglist_buttons = gears.table.join(
   awful.button({}, 1, function(t) t:view_only() end), awful.button(
     {modkey}, 1,
@@ -169,7 +156,6 @@ local tasklist_buttons = gears.table.join(
 )
 
 local function set_wallpaper(s)
-  -- Wallpaper
   if beautiful.wallpaper then
     local wallpaper = beautiful.wallpaper
     -- If wallpaper is a function, call it with the screen
@@ -178,22 +164,18 @@ local function set_wallpaper(s)
   end
 end
 
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
+
 screen.connect_signal("property::geometry", set_wallpaper)
+
 
 awful.screen.connect_for_each_screen(
   function(s)
-    -- Wallpaper
     set_wallpaper(s)
-
-    -- Each screen has its own tag table.
     awful.tag(
       {"1", "2", "3", "4", "5", "6", "7", "8", "9"}, s,
       awful.layout.layouts[1]
     )
-
-    -- Dropdown terminal https://gkaczorek.com/2020-09-25-adding-an-alacritty-based-drop-down-terminal-in-awesomewm/
-    s.quake = quake(
+    s.quake = lain.util.quake(
       {
         app = "/usr/bin/urxvt",
         argname = "-title %s",
@@ -206,11 +188,7 @@ awful.screen.connect_for_each_screen(
         border = 2,
       }
     )
-
-    -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
-    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
     s.mylayoutbox = awful.widget.layoutbox(s)
     s.mylayoutbox:buttons(
       gears.table.join(
@@ -225,24 +203,17 @@ awful.screen.connect_for_each_screen(
         )
       )
     )
-    -- Create a taglist widget
     s.mytaglist = awful.widget.taglist {
       screen = s,
       filter = awful.widget.taglist.filter.all,
       buttons = taglist_buttons,
     }
-
-    -- Create a tasklist widget
     s.mytasklist = awful.widget.tasklist {
       screen = s,
       filter = awful.widget.tasklist.filter.currenttags,
       buttons = tasklist_buttons,
     }
-
-    -- Create the wibox
     s.mywibox = awful.wibar({position = "top", screen = s})
-
-    -- Add widgets to the wibox
     s.mywibox:setup{
       layout = wibox.layout.align.horizontal,
       { -- Left widgets
@@ -262,9 +233,6 @@ awful.screen.connect_for_each_screen(
     }
   end
 )
--- }}}
-
--- {{{ Mouse bindings
 root.buttons(
   gears.table.join(
     awful.button({}, 3, function() mymainmenu:toggle() end),
@@ -272,9 +240,6 @@ root.buttons(
     awful.button({}, 5, awful.tag.viewprev)
   )
 )
--- }}}
-
--- {{{ Key bindings
 globalkeys = gears.table.join(
   awful.key(
     {modkey}, "s", hotkeys_popup.show_help,
@@ -336,11 +301,11 @@ globalkeys = gears.table.join(
     end, {description = "go back", group = "client"}
   ), -- Standard program
   awful.key(
-    {"Mod1"}, "Tab", function() awful.spawn('rofi -show windowcd -modi windowcd') end, 
+    {"Mod1"}, "Tab", function() awful.spawn('rofi -show windowcd -modi windowcd') end,
     {description = "open programs", group = "client"}
   ),
   awful.key(
-    {"Mod1", "Shift"}, "Tab", function() awful.spawn('rofi -show window -modi window') end, 
+    {"Mod1", "Shift"}, "Tab", function() awful.spawn('rofi -show window -modi window') end,
     {description = "open programs", group = "client"}
   ),
   awful.key(
@@ -461,8 +426,6 @@ clientkeys = gears.table.join(
   ),
   awful.key(
     {modkey}, "n", function(c)
-      -- The client currently has the input focus, so it cannot be
-      -- minimized, since minimized clients can't have the focus.
       c.minimized = true
     end, {description = "minimize", group = "client"}
   ),
@@ -486,9 +449,6 @@ clientkeys = gears.table.join(
   )
 )
 
--- Bind all key numbers to tags.
--- Be careful: we use keycodes to make it work on any keyboard layout.
--- This should map on the top row of your keyboard, usually 1 to 9.
 for i = 1, 9 do
   globalkeys = gears.table.join(
     globalkeys, -- View tag only.
@@ -552,14 +512,9 @@ clientbuttons = gears.table.join(
   )
 )
 
--- Set keys
 root.keys(globalkeys)
--- }}}
 
--- {{{ Rules
--- Rules to apply to new clients (through the "manage" signal).
 awful.rules.rules = {
-  -- All clients will match this rule.
   {
     rule = {},
     properties = {
@@ -593,9 +548,6 @@ awful.rules.rules = {
         "veromix",
         "xtightvncviewer",
       },
-
-      -- Note that the name property shown in xprop might be set slightly after creation of the client
-      -- and the name shown there might not match defined rules here.
       name = {
         "Event Tester", -- xev.
       },
@@ -611,33 +563,19 @@ awful.rules.rules = {
     rule_any = {type = {"normal", "dialog"}},
     properties = {titlebars_enabled = false},
   },
-
--- Set Firefox to always map on the tag named "2" on screen 1.
--- { rule = { class = "Firefox" },
---   properties = { screen = 1, tag = "2" } },
 }
--- }}}
 
--- {{{ Signals
--- Signal function to execute when a new client appears.
 client.connect_signal(
   "manage", function(c)
-    -- Set the windows at the slave,
-    -- i.e. put it at the end of others instead of setting it master.
-    -- if not awesome.startup then awful.client.setslave(c) end
-
     if awesome.startup and not c.size_hints.user_position and
       not c.size_hints.program_position then
-      -- Prevent clients from being unreachable after screen count changes.
       awful.placement.no_offscreen(c)
     end
   end
 )
 
--- Add a titlebar if titlebars_enabled is set to true in the rules.
 client.connect_signal(
   "request::titlebars", function(c)
-    -- buttons for the titlebar
     local buttons = gears.table.join(
       awful.button(
         {}, 1, function()
@@ -656,7 +594,6 @@ client.connect_signal(
         end
       )
     )
-
     awful.titlebar(c):setup{
       { -- Left
         awful.titlebar.widget.iconwidget(c),
@@ -684,7 +621,6 @@ client.connect_signal(
   end
 )
 
--- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal(
   "mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
@@ -697,9 +633,7 @@ client.connect_signal(
 client.connect_signal(
   "unfocus", function(c) c.border_color = beautiful.border_normal end
 )
--- }}}
 
--- Autorun programs
 autorun = true
 autorunApps = {
   "/usr/bin/conky",
